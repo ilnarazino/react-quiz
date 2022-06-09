6947ce84a6dc24fe1fa104dc75239d09905b0d66
ilnar <ilnarazino@yandex.ru>
2022-06-09 19:12:23 +0300

    First commit

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,22 @@
+
+import React, { Component } from "react";
+import classes from './Quiz.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+
+class Quiz extends Component {
+    state = {
+        quiz: []
+    }
+    render() {
+        return (
+            <div className={classes.Quiz}>
+
+                <div className={classes.QuizWrapper}>
+                    <h1>Quiz</h1>
+                    <ActiveQuiz />
+                </div>
+            </div>)
+    }
+}
+
+export default Quiz
